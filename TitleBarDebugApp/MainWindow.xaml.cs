33feebdbfbeb3c8429c99c628380b18fcf295c00
Diego Lima <diegolima_nofx@hotmail.com>
2018-08-12 00:13:01 -0300

    Added debug app for the title bar.

---
+++ TitleBarDebugApp/MainWindow.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using MyControlsLibrary;
+
+namespace TitleBarDebugApp
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            initTitleBar();
+        }
+
+        private void initTitleBar()
+        {
+            CustomTitleBar titleBar = new CustomTitleBar(this, mainGrid);
+            titleBar.Text = "Comments";
+            titleBar.TextAlignment = HorizontalAlignment.Left;
+            titleBar.Height = 24;
+            titleBar.MinimizeButtonVisibility = Visibility.Collapsed;
+            titleBar.IsAutoHide = true;
+            titleBar.AutoHideDelay = 2000;
+            titleBar.IsPlayAnimation = true;
+            titleBar.AnimationInterval = 25;
+
+            //parent window configurations
+            //commentsTxtB.Margin = new Thickness(0, titleBar.Height, 0,0);
+        }
+    }
+}
