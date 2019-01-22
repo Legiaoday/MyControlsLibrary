53f0f83f6d49328ea47cd9696c7cababafc5f525
Diego Lima <diegolima_nofx@hotmail.com>
2019-01-22 20:28:00 -0200

    ...

---
+++ MyControlsLibrary/NumericUpDown.xaml.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MyControlsLibrary
+{
+    public partial class NumericUpDown : UserControl
+    {
+        public NumericUpDown()
+        {
+            InitializeComponent();
+        }
+
+        private void NumberTxt_PreviewTextInput(object sender, TextCompositionEventArgs e)
+        {
+
+        }
+
+        private void NumberTxt_PreviewKeyDown(object sender, KeyEventArgs e)
+        {
+
+        }
+
+        private void NumberTxt_PreviewExecuted(object sender, ExecutedRoutedEventArgs e)
+        {
+
+        }
+
+        private void upButton_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                int temp = Int32.Parse(numberTxt.Text);
+
+                temp++;
+                numberTxt.Text = temp.ToString();
+            }
+            catch (FormatException ex)
+            {
+                numberTxt.Text = "1";
+            }
+        }
+
+
+        private void downButton_Click(object sender, RoutedEventArgs e) 
+        {
+            try
+            {
+                int temp = Int32.Parse(numberTxt.Text);
+
+                if (temp > 0)
+                {
+                    temp--;
+                    numberTxt.Text = temp.ToString();
+                }
+            }
+            catch (FormatException ex)
+            {
+                numberTxt.Text = "0";
+            }
+        }
+    }
+}
